887e6619ec151b72fc5df1648d7429dd0a5bd6a7
Igor Eduardo <igorercl@gmail.com>
2023-12-30 00:59:23 -0300

    concluido!!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine(" Smartphone Nokia: ");
+Smartphone nokia  = new Nokia("81 973454619", "Nokia C2 2ND Edition","75412365897452147",128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("whatsapp");
+
+Console.WriteLine("-------------------------------------------------------------------------");
+
+Console.WriteLine(" Smatphone Iphone: ");
+Smartphone iphone = new Iphone("81 973454619", "IPHONE 15PRO MAX SILVER","785452197452147",256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
